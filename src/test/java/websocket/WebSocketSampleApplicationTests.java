e20a870b01c2b740108f54b2a33301ec7e847cae
sakuoden <4oden18@gmail.com>
2022-03-21 00:40:28 +0900

    first commit

---
+++ src/test/java/websocket/WebSocketSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
